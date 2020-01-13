5fcae5485f0cc3bcd801ef01d91696c2b748bcfd
Richard Gary <RGARY@cityu.edu>
2020-01-12 16:33:57 -0800

    Submission for Module 01

---
+++ ON/RichardGary/Module 01/MultiDimensionalArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sticking with "Hello World!"
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
