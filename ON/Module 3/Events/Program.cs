3ffeaa0d648383bc0a07953672d7a772c1d7c6fb
Nicolas Conti <continicolas@cityuniversity.edu>
2020-01-26 13:15:54 -0800

    Submission for Module 3

---
+++ ON/Module 3/Events/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Events
+{
+    public delegate string MyDel(string str);
+    class Program
+    {
+        event MyDel MyEvent;
+
+        public Program()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+        }
+        public string WelcomeUser(string username)
+        {
+            return "Welcome " + username;
+        }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            string result = obj1.MyEvent("This is my first evemt");
+            Console.WriteLine(result);
+        }
+    }
+}
