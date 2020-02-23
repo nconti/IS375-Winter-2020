b25f1d44625158ae410dbd73f9f9e9eb9d8f08a1
Nicolas Conti <continicolas@cityuniversity.edu>
2020-02-23 15:38:49 -0800

    Submission for Module 7

---
+++ ON/Module 7/DBConnection/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+            MessageBox.Show("Connection Open Successfully!");
+            myconnection.Close();
+        }
+    }
+}
