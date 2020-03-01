df47599de1953a0e0500abe863c474059e9dba89
Nicolas Conti <continicolas@cityuniversity.edu>
2020-03-01 09:48:07 -0800

    Submission for Module 8

--- ON/Module 7/DBConnection/Form1.Designer.cs
+++ ON/Module 7/DBConnection/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(338, 187);
+            this.button1.Location = new System.Drawing.Point(338, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 40);
             this.button1.TabIndex = 0;
@@ -41,11 +44,44 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(338, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +92,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ON/Module 7/DBConnection/Form1.cs
+++ ON/Module 7/DBConnection/Form1.cs
@@ -22,10 +22,110 @@ namespace DBConnection
         {
             string connectionString;
             SqlConnection myconnection;
-            connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            connectionString = "Data Source=DESKTOP-H0K0QNL;Initial Catalog=ContiNicStoreDB;Integrated Security=True;Pooling=False";
             myconnection = new SqlConnection(connectionString);
             myconnection.Open();
-            MessageBox.Show("Connection Open Successfully!");
+            // MessageBox.Show("Connection Open Successfully!");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            // define SQL statement
+            sql = "Select ID,Name from Student";
+            // the command statement used to perform operations of reading and writing into the database
+            command = new SqlCommand(sql, myconnection);
+            // define the reader used to get all the data specified by the SQL query
+            dataReader = command.ExecuteReader();
+
+            // set the table values
+            while(dataReader.Read())
+            {
+                Output += dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+            // good practice to close all the objects related to our database operation
+            dataReader.Close();
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Data Source=DESKTOP-H0K0QNL;Initial Catalog=ContiNicStoreDB;Integrated Security=True;Pooling=False";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+
+            // SQLDataAdapter used to perform specific SQL operations such as insert, delete and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Insert into Student (ID,Name) values(4, 'Jack' )";
+            MessageBox.Show("Added Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.InsertCommand = new SqlCommand(sql, myconnection);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Data Source=DESKTOP-H0K0QNL;Initial Catalog=ContiNicStoreDB;Integrated Security=True;Pooling=False";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+
+            //SqlDataAdapter used to perform specific SQL operations such as insert, delete and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Update Student set Name='Jones' where ID=4";
+            MessageBox.Show("Updated Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.UpdateCommand = new SqlCommand(sql, myconnection);
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Data Source=DESKTOP-H0K0QNL;Initial Catalog=ContiNicStoreDB;Integrated Security=True;Pooling=False";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+
+            //SqlDataAdapter used to perform specific SQL operations such as insert, delete and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Delete Student where Name='Jones'";
+            MessageBox.Show("Deleted Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.DeleteCommand = new SqlCommand(sql, myconnection);
+            adapter.DeleteCommand.ExecuteNonQuery();
+
+            command.Dispose();
             myconnection.Close();
         }
     }
