c70d0c20c78e5a1444e0f7bf68e8357a982c4427
Nicolas Conti <continicolas@cityuniversity.edu>
2020-03-14 12:11:59 -0700

    Submission for Module 10

---
+++ ON/Module 10/FormValidation/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormValidation.Entities
+{
+    public class Account
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})")]
+        public string Password { get; set; }
+        
+        [Range(18, 60)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ ON/Module 10/FormValidation/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FormValidation.Entities;
+
+namespace FormValidation
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Account account { get; set; }
+        public void OnGet()
+        {
+            account = new Account();
+        }
+        public IActionResult OnPost(Account account)
+        {
+            if (account.Username != null&& account.Username.Equals("abc"))
+            {
+                ModelState.AddModelError("account.Username", "Username Already Exists");
+            }
+            
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("Success");
+            }
+        }
+    }
+}
